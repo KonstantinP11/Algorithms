5c150737a24d200673c099ca81fa74420aee6f3c
conditional <105pkn@mail.ru>
2023-10-20 17:19:47 +0700

    algoritms3

--- src/main/java/org/example/service/IntegerListImpl.java
+++ src/main/java/org/example/service/IntegerListImpl.java
@@ -115,7 +115,7 @@ public class IntegerListImpl implements IntegerList {
     private void validateSize() {
         if (size == storage.length) {
 //            throw new StorageFullException();
-            grow(size);
+            grow();
         }
     }
 
@@ -200,8 +200,42 @@ public class IntegerListImpl implements IntegerList {
         return false;
     }
 
-    private Integer grow(int size) {
-        size = (int) (1.5 * size);
-        return size;
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
+    private void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swapElements(Integer[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    private void sort(Integer[] arr) {
+        quickSort(arr, 0, arr.length - 1);
     }
 }
